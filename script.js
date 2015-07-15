81ad3910c171b67a82c89fb790649ee065de060b
Ryan Kahle <jrkahle@hotmail.com>
2015-07-15 10:42:12 -0400

    Resaving my master, non-object

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
+var timeKeeper;
 
-  var timeKeeper;
+var cards = document.querySelectorAll(".pieceBack");
 
-  var cards = document.querySelectorAll(".pieceBack");
-
-  var randomTracker = [];
+var randomTracker = [];
 
 
 
@@ -11,86 +10,86 @@
 
 
 // Create a onclick event to rotate the squares
-  $(".pieceContainer" ).on( "click" , function() {
-    $( this ).addClass( "flip" );
+$(".pieceContainer" ).on( "click" , function() {
+  $( this ).addClass( "flip" );
 });
 
-  $( ".pieceContainer" ).on( "click" , time );
+$( ".pieceContainer" ).on( "click" , time );
 
-  $( "button" ).on( "click" , clear );
-
-
-  function time() {
-    timeKeeper = setTimeout(compareFlipped, 500 );
-  }
+$( "button" ).on( "click" , clear );
 
 
-  // using Math, chooses randomly how to set up board
+function time() {
+  timeKeeper = setTimeout(compareFlipped, 500 );
+}
 
 
-  function randomCounter() {
+// using Math, chooses randomly how to set up board
 
-    while(randomTracker.length < 10) {
-      var randomNum = Math.floor((Math.random() * 10) + 1);
-      var found = false;
 
-      for(var i = 0; i < randomTracker.length; i++) {
-        if (randomTracker[i] == randomNum) {found=true; break}
-      }
+function randomCounter() {
 
-      if (!found)randomTracker[randomTracker.length] = randomNum;
-      }
+  while(randomTracker.length < 10) {
+    var randomNum = Math.floor((Math.random() * 10) + 1);
+    var found = false;
 
-  } // end of randomCounter
+    for(var i = 0; i < randomTracker.length; i++) {
+      if (randomTracker[i] == randomNum) {found=true; break}
+    }
+
+    if (!found)randomTracker[randomTracker.length] = randomNum;
+    }
+
+} // end of randomCounter
 
 randomCounter();  // set randomTracker to mix up square data-ids
 
 function setSquare() {
 
-  for(var i = 0; i < cards.length; i++ ) {
-  var counter = 0;
-    if (randomTracker.length == 0) {
-      randomCounter();
-    }
-
-    counter = randomTracker.pop();
-    cards[i].setAttribute( "data-id", counter);
+for(var i = 0; i < cards.length; i++ ) {
+var counter = 0;
+  if (randomTracker.length == 0) {
+    randomCounter();
   }
+
+  counter = randomTracker.pop();
+  cards[i].setAttribute( "data-id", counter);
+}
 } // end setSquare
 
 setSquare(); // puts background-image in squares
 
 function compareFlipped() {
-  clearTimeout(timeKeeper);
+clearTimeout(timeKeeper);
 
-  var flipped = $( ".flip" );
+var flipped = $( ".flip" );
 
-  if(flipped.length == 2) {
-    var flipOne = $( ".flip .pieceBack" ).eq(0).attr("data-id");
+if(flipped.length == 2) {
+  var flipOne = $( ".flip .pieceBack" ).eq(0).attr("data-id");
 
-    var flipTwo = $( ".flip .pieceBack" ).eq(1).attr("data-id");
+  var flipTwo = $( ".flip .pieceBack" ).eq(1).attr("data-id");
 
-    if(flipOne == flipTwo) {
-      $( ".flip" ).addClass( "stayFlipped" );
-      $( ".pieceContainer" ).removeClass( "flip" );
-      winner();
-    } else if (flipOne != flipTwo ) {
-      $( ".pieceContainer" ).removeClass( "flip" );
-    }
+  if(flipOne == flipTwo) {
+    $( ".flip" ).addClass( "stayFlipped" );
+    $( ".pieceContainer" ).removeClass( "flip" );
+    winner();
+  } else if (flipOne != flipTwo ) {
+    $( ".pieceContainer" ).removeClass( "flip" );
   }
+}
 
 } // end compareFlipped;
 
 function winner() {
-  var winner = $( ".stayFlipped");
+var winner = $( ".stayFlipped");
 
-  if(winner.length == 20) {
-    alert("Winner winner chicken dinner!");
-  }
+if(winner.length == 20) {
+  alert("Winner winner chicken dinner!");
 }
- function clear() {
-   $( ".pieceContainer").removeClass( "stayFlipped" );
-   $( ".pieceBack" ).removeAttr("data-id");
-   randomCounter();
-   setSquare();
- }
+}
+function clear() {
+ $( ".pieceContainer").removeClass( "stayFlipped" );
+ $( ".pieceBack" ).removeAttr("data-id");
+ randomCounter();
+ setSquare();
+}
